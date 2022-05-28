f3c7e31dcc7317d1c8a3692e75579551f854eae2
Buse Özmen <106406375+buseozmen20@users.noreply.github.com>
2022-05-29 00:27:58 +0300

    Bildirim ve Resepsiyon ekranı tasarım ve kodlaması

---
+++ otel/BildirimEkran.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace otel_deneme_1
+{
+    partial class BildirimEkran
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tblodabilgiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button13 = new System.Windows.Forms.Button();
+            this.txt_bildirim = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.tblodabilgiBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(33, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Oda Servisini Odaya Gönderiniz.";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(33, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(375, 84);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Acil Sağlık Ekibini Odaya Yönlendiriniz";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(33, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(375, 77);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Çarşaf ve Nevresimlerimin Değiştirilmesini Talep Ediyorum";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(33, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(375, 77);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Teknik Ekibi Odaya Yönlendiriniz";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(33, 367);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(375, 77);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Buzdolabının Doluluk Durumunu Güncelleyiniz";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(33, 459);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(375, 77);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Otel Önüne Taksi Çağırınız";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(430, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(375, 77);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Havalimanı Transfer Ekibini Otel Önüne Çağırınız";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(430, 102);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(375, 84);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Yemek Menüsü Hakkında Bilgi Edinmek İstiyorum";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(430, 192);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(375, 77);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "Aracımın Otel Önüne Getirilmesini Talep Ediyorum ";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Transparent;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.ForeColor = System.Drawing.Color.Black;
+            this.button10.Location = new System.Drawing.Point(430, 275);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(375, 77);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "Kayıp Eşya Bildirimi";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Transparent;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button11.ForeColor = System.Drawing.Color.Black;
+            this.button11.Location = new System.Drawing.Point(430, 367);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(375, 77);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "Bugün ki Otel Etkinlikleri Nelerdir?";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Transparent;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button12.ForeColor = System.Drawing.Color.Black;
+            this.button12.Location = new System.Drawing.Point(430, 459);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(375, 77);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "Misafirim Gelicek Lütfen Odama Yönlendiriniz";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(838, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(890, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Oda Seç";
+            // 
+            
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.White;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button13.ForeColor = System.Drawing.Color.Green;
+            this.button13.Location = new System.Drawing.Point(838, 102);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(200, 156);
+            this.button13.TabIndex = 3;
+            this.button13.Text = "Bildirim Gönder";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // txt_bildirim
+            // 
+            this.txt_bildirim.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_bildirim.Location = new System.Drawing.Point(33, 564);
+            this.txt_bildirim.Name = "txt_bildirim";
+            this.txt_bildirim.Size = new System.Drawing.Size(772, 45);
+            this.txt_bildirim.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(838, 275);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // BildirimEkran
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1113, 675);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txt_bildirim);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BildirimEkran";
+            this.Text = "Bildirim Gönder";
+            this.Load += new System.EventHandler(this.BildirimEkran_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.tblodabilgiBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource tblodabilgiBindingSource;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox txt_bildirim;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ otel/BildirimEkran.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace otel_deneme_1
+{
+    public partial class BildirimEkran : Form
+    {
+        public BildirimEkran()
+        {
+            InitializeComponent();
+
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=LAPTOP-S6N20HE1\\SQLEXPRESS07;Initial Catalog = otelotomasyon; Integrated Security = True");
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+        public string SqlCon = "Data Source=LAPTOP-S6N20HE1\\SQLEXPRESS07;Initial Catalog = otelotomasyon; Integrated Security = True";
+
+    
+
+        private void BildirimEkran_Load_1(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select *from tbl_odabilgi ", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                comboBox1.Items.Add(read["odanumarası"]);
+
+            }
+            baglanti.Close();
+        }
+        //Bildirimler Adlı DataGride Veri Gönderme İşlemi 
+        private void button13_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(SqlCon);
+            string sql = "insert into tbl_bildirimler(OdaNo,Bildirimler,BildirimTarihi)values(@OdaNoo,@Bildirimlerr,@BildirimTarihii)";
+            cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@OdaNoo", comboBox1.Text);
+            cmd.Parameters.AddWithValue("@Bildirimlerr", txt_bildirim.Text);
+            cmd.Parameters.AddWithValue("@BildirimTarihii", dateTimePicker1.Value);
+
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Bildirim Gönderildi.");
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             txt_bildirim.Text = button1.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button11.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button5.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button4.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button3.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button2.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button12.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button6.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button10.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button9.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button8.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txt_bildirim.Text = button7.Text;
+        }
+    }
+}
